e76520649457b31603c7c4803cc88eaf21936e8f
dohliam <dohliam@gmail.com>
2026-05-31 16:31:56 -0700

    pingyam - update

---
+++ pingyam/js/pingyam.js
@@ -0,0 +1,59 @@
+function yuepinzh(v) {
+  text = document.getElementById("converttext").value;
+  frompin = document.getElementById("frompin").value;
+  topin = document.getElementById("topin").value;
+  results = document.getElementById("results");
+  container = document.getElementById("rcontainer");
+  text = text.replace(/\n/g, " <br> ");
+  punct = /[,\.\?\!@#\$%\^&\*\(\)\[\]\{\}":;~\-\+=<>]+/g;
+  ptext = text.replace(punct, " @@@$&@@@ ");
+  textsplit = ptext.split(/\s/);
+  resarray = [];
+  for (var i = 0; i < textsplit.length; i++) {
+    syll = textsplit[i];
+    ispy = false;
+    if (!/@/.test(syll)) {
+      for (var key in yuepin) {
+        if (yuepin.hasOwnProperty(key)) {
+          line = yuepin[key];
+          linesplit = line.split(',');
+          source = linesplit[frompin];
+          target = linesplit[topin];
+          if (source == syll.toLowerCase()) {
+            resarray.push(target);
+            ispy = true;
+          }
+        }
+      }
+    }
+    if (ispy != true) {
+      resarray.push(syll);
+    }
+  }
+  container.style.display = "";
+  results.value = resarray.join(" ").replace(/\s?@@@\s?/g, "").replace(/ ?<br> ?/g, "\n");
+}
+
+function translate_string(lang) {
+  strings = document.getElementsByClassName("tr-string");
+  for (var i = 0; i < strings.length; i++) {
+    current = strings[i];
+    string_id = current.dataset.tr;
+    text = current.textContent;
+    replace_string = lang_strings[lang][string_id];
+    if (current.placeholder) {
+      current.placeholder = replace_string;
+    } else {
+      current.innerHTML = replace_string;
+    }
+  }
+}
+
+function switchDir() {
+  frompin_el = document.getElementById("frompin");
+  topin_el = document.getElementById("topin");
+  frompin = frompin_el.value;
+  topin = topin_el.value;
+  frompin_el.value = topin;
+  topin_el.value = frompin;
+}
